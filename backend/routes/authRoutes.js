8b2a94adad9a81981aba1dff0c11161ebe5c4bf4
sanju234-san <sanjeevnidhir05@gmail.com>
2025-08-24 16:44:47 +0530

    ..

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,16 +1,10 @@
-import jwt from 'jsonwebtoken';
+import express from 'express';
+import { register, login } from '../controllers/authController.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
-export const authMiddleware = (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
-    }
+const router = express.Router();
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: "Unauthorized" });
-        }
-        req.user = decoded;
-        next();
-    });
-};
+router.post("/register", register);
+router.post("/login", authMiddleware, login);
+
+export default router;
